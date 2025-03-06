d0b67d26437225032c3c8ee4529f7c55f0350f81
OperaMauve <165987157+OperaMauve@users.noreply.github.com>
2025-03-06 11:20:08 +1100

    Update Data.c

--- src/Data.c
+++ src/Data.c
@@ -1,2 +1,11 @@
 #include "..\include\SML.h"
 
+// Wrapped Stack
+
+void createStack(Stack *output){
+    output = (Stack*)malloc(output, sizeof(Stack));
+    if (output == NULL) {
+    	printf("Out of memory defining Stack");
+    }
+    output->element = (char*)malloc(
+}
