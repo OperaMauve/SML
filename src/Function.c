57b974d7d5ace3d86f0a8c75b06afb00ff792b9e
OperaMauve <165987157+OperaMauve@users.noreply.github.com>
2025-03-17 12:43:02 +1100

    silly commit
    up to nothing

--- src/Function.c
+++ src/Function.c
@@ -2,15 +2,98 @@
 
 // Lexer (Infix >>> Polish)
 
-void lexer(char* instruction, char* output){
-    output = (char*)malloc(sizeof(instruction));
+void lexer(Stack* instruction, Stack* output){
+
+    Stack* buffer = (Stack*)malloc(sizeof(Stack));
     
+    // Load initial value
+    buffer->element = (char*)malloc(sizeof(char));
+    output->element = (char*)malloc(sizeof(char));
+
+    // Set top to negative one
+    buffer->top = -1;
+    output->top = -1;
+
+    int max = instruction->top;
+    int prevWeight = 0;
+
+    for (int i = 0; i < max; i++){
+        if (isNumber(instruction->element[i])){
+            // push to the output
+            output->top += 1;
+            output->element = (char*)realloc(output->element, sizeof(char)*(output->top+1));
+            output->element[output->top] = instruction->element[i];
+        } else if(lexerPriority(instruction->element[i]) == 3){
+            // Bracket starts
+            i++;
+            // Start new input stack
+            Stack* input = (Stack*)malloc(sizeof(Stack));
+            input->element = (char*)malloc(sizeof(char));
+            input->top = 0;
+            // Define bracket output stack
+            Stack* bracOut = (Stack*)malloc(sizeof(Stack));
+            
+            // Add all elements within the bracket to a new input stack
+            while(lexerPriority(instruction->element[i]) != 3){
+                input->top += 1;
+                input->element = (char*)realloc(input->element, sizeof(char)*(input->top+1)); 
+                input->element[input->top] = instruction->element[i]; 
+                i++;
+            }
+            i--;
+
+            input->top -= 1;
+
+            // Add recursion to infix the output.
+            lexer(input, bracOut);
+
+            // Push BracOut into Output
+            for (int j = 0; j < bracOut->top; j++){\
+                output->top += 1;
+                output->element = (char*)realloc(output->element, sizeof(char)*(output->top+1));
+                output->element[output->top] = bracOut->element[j];
+            }
+
+            // Free BracOut
+            free(bracOut->element);
+            free(bracOut);
+            prevWeight = 0;
+        } else if(lexerPriority < prevWeight) {
+            // Push buffer to output
+            for (int j = 0; j < buffer->top; j++){
+                output->top += 1;
+                output->element = (char*)realloc(output->element, sizeof(char)*(output->top+1));
+                output->element[output->top] = buffer->element[j];
+            }
+
+            // Free the Buffer
+            free(buffer->element);
+            free(buffer);
+
+            // Renew the Buffer
+            Stack* buffer = (Stack*)malloc(sizeof(Stack));
+            buffer->element = (char*)malloc(sizeof(char));
+            buffer->top = -1;
+
+            // Push element into buffer
+            buffer->top += 1;
+            buffer->element = (char*)realloc(buffer->element, sizeof(char)*(buffer->top+1));
+            buffer->element[buffer->top] = instruction->element[i];
+            
+            prevWeight = lexerPriority(instruction->element[i]);
+        } else {
+            // Add to buffer
+            buffer->top += 1;
+            buffer->element = (char*)realloc(buffer->element, sizeof(char)*(buffer->top+1));
+            buffer->element[buffer->top] = instruction->element[i];
+            prevWeight = lexerPriority(instruction->element[i]);
+        }
+    }
 }
 
 void applyFunction(Matrix* matrix, char *function){
     // Apply function to each element on the matrix
     //lexer
-    instructions* instruct = (instructions*)malloc(sizeof(instructions));
 
     for (int i = 0; i < sizeof(function)/sizeof(char); i++){
         // categories characters, and then if there is a different category of characters in the next character, then switch to the next token
@@ -22,19 +105,6 @@ void applyFunction(Matrix* matrix, char *function){
     }
 }
 
-int precedence(char character){
-    if (character == '(' || character == ')'){
-        return 0;        
-    }
-    if (character == '/' || character == '*'){
-        return 1;        
-    }
-    if (character == '+' || character == '-'){
-        return 2;        
-    }
-    printf("Invalid Syntax");
-    return 0;
-}
 
 int categorise(char character){
     if (isNumber(character)){
@@ -52,3 +122,18 @@ int categorise(char character){
     printf("%s is not recognised", character);
     return -1;
 }
+
+int lexerPriority(char a){
+    // Returns the priority of the binary operand
+    if (a == '+' || a == '-'){
+        return 1;
+    }
+    if (a == '*' || a == '/'){
+        return 2;
+    }
+    if (a == '(' || a == ')'){
+        return 3;
+    }
+    printf("Invalid Syntax");
+    return -1;
+} 
